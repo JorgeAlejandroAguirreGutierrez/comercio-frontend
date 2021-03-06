6a226e7217890b8785ede5ec59a7b832696e4029
alejandro <JAGUIR4@bancodebogota.com.co>
2021-03-05 21:29:53 -0500

    mod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   host:'https://comercio-backend.herokuapp.com',
   prefijo_url_img: 'https://comercio-backend.herokuapp.com/img/',
-  prefijo_url_imagqr: 'https://comercio-backend.herokuapp.com/imgqr/',
+  prefijo_url_imgqr: 'https://comercio-backend.herokuapp.com/imgqr/',
   prefijo_url_imgfront: 'https://comercio-backend.herokuapp.com/imgfront/'
 };
